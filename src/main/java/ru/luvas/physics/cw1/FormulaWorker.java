6562545bfeee4fb59906c7af7270578784bbb70a
RinesThaix <iam@kostya.sexy>
2016-11-28 16:22:27 +0300

    Более живописное изменение радиусов в зависимости от длины световой волны

--- src/main/java/ru/luvas/physics/cw1/FormulaWorker.java
+++ src/main/java/ru/luvas/physics/cw1/FormulaWorker.java
@@ -19,10 +19,11 @@ public class FormulaWorker {
     private final FilledRectangle firstHole, secondHole;
     private final SemiCircle[] firstHoleCircles, secondHoleCircles;
     private final Line[] lightLines;
-    private Color lightColor = Color.blue;
-    private int currentLightWave = PHYSICS_DEFAULT_LIGHT_WAVE_LENGTH;
+    private Color lightColor;
+    private int currentLightWave;
 
     FormulaWorker() {
+        changeLight(PHYSICS_LIGHT_WAVE_LENGTH_MIN);
         lattice = new FilledRectangle(GRAPHICS_LATTICE_START_X, GRAPHICS_LATTICE_START_Y,
                 GRAPHICS_LATTICE_WIDTH, GRAPHICS_LATTIE_LENGTH, Color.black);
         firstHole = new FilledRectangle(GRAPHICS_LATTICE_START_X, GRAPHICS_FIRST_HOLE_START_Y,
@@ -50,7 +51,7 @@ public class FormulaWorker {
         
         Utils.schedule(() -> {
 //            PHYSICS_DISTANCE_BETWEEN_HOLES += 1d * Math.pow(10, -5);
-            changeLight(currentLightWave += 10);
+            changeLight(currentLightWave + 10);
             recalculate();
         }, 1, 1, TimeUnit.SECONDS);
     }
@@ -82,13 +83,17 @@ public class FormulaWorker {
         }
     }
     
-    public void changeLight(int newWaveLenght) {
+    public void changeLight(int newWaveLength) {
+        newWaveLength = (int) Math.min(PHYSICS_LIGHT_WAVE_LENGTH_MAX, Math.max(PHYSICS_LIGHT_WAVE_LENGTH_MIN, newWaveLength));
+        double wavePercentage = GRAPHICS_LIGHT_WAVE_PERCENTAGE_MODIFIER * (newWaveLength - PHYSICS_LIGHT_WAVE_LENGTH_MIN) / (PHYSICS_LIGHT_WAVE_LENGTH_MAX - PHYSICS_LIGHT_WAVE_LENGTH_MIN);
+        wavePercentage += GRAPHICS_LIGHT_WAVE_PERCENTAGE_BASEMENT;
         //changing color
-        lightColor = ColoursMapping.getClosestColor(newWaveLenght);
+        lightColor = ColoursMapping.getClosestColor(newWaveLength);
         //changing radiuses
-        GRAPHICS_FIRST_CIRCLE_RADIUS = GRAPHICS_DEFAULT_FIRST_CIRCLE_RADIUS * newWaveLenght / PHYSICS_DEFAULT_LIGHT_WAVE_LENGTH;
+        GRAPHICS_FIRST_CIRCLE_RADIUS = (int) (GRAPHICS_DEFAULT_FIRST_CIRCLE_RADIUS * wavePercentage);
         //changing line deltas
-        GRAPHICS_LIGHT_LINE_DELTA = GRAPHICS_DEFAULT_LIGHT_LINE_DELTA * newWaveLenght / PHYSICS_DEFAULT_LIGHT_WAVE_LENGTH;
+        GRAPHICS_LIGHT_LINE_DELTA = (int) (GRAPHICS_DEFAULT_LIGHT_LINE_DELTA * wavePercentage);
+        this.currentLightWave = newWaveLength;
     }
     
 }
